c7008c71b0e3c205fa917541cc09d1248d979de2
omit-excelitai <omit2023@excelitai.com>
2023-04-29 15:51:56 +0600

    Update

--- lib/data/repositories/order_details_repo.dart
+++ lib/data/repositories/order_details_repo.dart
@@ -1,5 +1,9 @@
 import 'package:bppshop_agent/data/datasource/remote/dio/dio_client.dart';
+import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../../utill/app_constants.dart';
+import '../datasource/remote/exception/api_error_handler.dart';
+import '../model/base_model/api_response.dart';
 
 class OrderDetailsRepo{
 
@@ -7,4 +11,15 @@ class OrderDetailsRepo{
   final SharedPreferences sharedPreferences;
 
   OrderDetailsRepo({required this.dioClient, required this.sharedPreferences});
+
+  /// For get order details data
+  Future<ApiResponse> getOrderDetailsData({required int? id}) async{
+    try{
+      Response response = await dioClient.get("${AppConstants.orderDetailsUrl}$id");
+      return ApiResponse.withSuccess(response);
+    }catch(e){
+      print(e.toString());
+      return ApiResponse.withError(ApiErrorHandler.getMessage(e));
+    }
+  }
 }
